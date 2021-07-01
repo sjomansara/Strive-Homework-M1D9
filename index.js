f7cd63450b5747abf4563f420f2cd6d87c3d8b21
sjomansara <sara.sjoman@hotmail.com>
2021-07-01 16:22:41 +0200

    added random numbers button

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
-const displayAllNumbers = function(){
+const displayAllNumbers = function() {
     let bingoContainerNode = document.getElementById("bingo-container")
     for (let bingoNumber = 1; bingoNumber <= 76; bingoNumber++) {
         let newBingoNumber = document.createElement("div") 
         newBingoNumber.innerText = bingoNumber
         newBingoNumber.classList.add("number")
-        // newBingoNumber.onclick = selectNumber
         bingoContainerNode.appendChild(newBingoNumber)
     }
 }
 window.addEventListener("load",function() { displayAllNumbers() })
+
+const randomNumber = function() {
+    let random = Math.random() * 76
+    random = Math.ceil(random)
+    let elements = document.getElementsByClassName("number")
+    let randomElement = elements[random]
+    randomElement.classList.add("selected")
+}
